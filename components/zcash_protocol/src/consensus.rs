3fcf37397b709d1d1d0afe1cd5a2e409d72f3795
Andrew Reece <azmreece@gmail.com>
2025-11-26 17:59:42 +0000

    Squashed 'librustzcash/' changes from 33dc74bf..e8758269
    e8758269 Fix obvious incompatibilities
    10c5fb34 Squashed commit of the following:
    a0879f43 Gui: Nuke lifetime insanity
    392523a7 WIP: faucet wallet sends to user wallet
    24a21d97 Merge commit '5abdc0860b55cd4dc41908b4dc168037005491fa' as 'librustzcash'
    1b6351a9 Merge commit '806cb235391791dfeee2107e2591554dd91f0099' as 'zebra-crosslink'
    806cb235 Squashed 'zebra-crosslink/' content from commit 311c73e9e
    3b8b29b2 reset
    35be5ee2 .
    2b876fb6 monorepo: zingo-infrastructure, zingolib, zaino - DONE
    55fa1eac monorepo: zaino - ALMOST DONE
    09c5dd4e monorepo: checkin zaino
    8901fdb0 monorepo: zingo-common - DONE
    50d6a4ac fix build errors
    2d092565 monorepo: checkin zingo-common
    923080e2 Update README.md
    23e6cf3c Update README.md
    b08cb594 Merge branch 'main' of github.com:ShieldedLabs/crosslink_monolith
    1273f6c6 pull in zebra version 311c73e9ead38987cd69eb999127d87eae6377d4
    8406ef92 Merge upstream changes from visualizer_zcash
    84351c0a .
    e60ae45b .
    60f93b15 monorepo: visualizer_zcash - DONE
    db5d1cbc monorepo: check in visualizer_zcash
    ae2e9366 monorepo: tenderlink DONE
    868d63f7 monorepo: check-in tenderlink master
    aba98820 monorepo: librustzcash DONE
    c802bfd3 monorepo: librustzcash
    52e24911 .
    f63cb1dc monorepo: check-in librustzcash
    01f1e579 .
    6758dc80 .
    54859be7 Update README.md
    ff40e43b Initial commit
    
    git-subtree-dir: librustzcash
    git-subtree-split: e875826969cf02149a60115059574551a745b280

--- components/zcash_protocol/src/consensus.rs
+++ components/zcash_protocol/src/consensus.rs
@@ -29,7 +29,7 @@ impl BlockHeight {
         BlockHeight(v)
     }
 
-    /// Subtracts the provided value from this height, returning `H0` if this would result in
+    /// Subtracts the provided value from this height, returning [`H0`] if this would result in
     /// underflow of the wrapped `u32`.
     pub fn saturating_sub(self, v: u32) -> BlockHeight {
         BlockHeight(self.0.saturating_sub(v))
@@ -432,7 +432,7 @@ impl Parameters for MainNetwork {
             NetworkUpgrade::Canopy => Some(BlockHeight(1_046_400)),
             NetworkUpgrade::Nu5 => Some(BlockHeight(1_687_104)),
             NetworkUpgrade::Nu6 => Some(BlockHeight(2_726_400)),
-            NetworkUpgrade::Nu6_1 => None,
+            NetworkUpgrade::Nu6_1 => Some(BlockHeight(3_146_400)),
             #[cfg(zcash_unstable = "nu7")]
             NetworkUpgrade::Nu7 => None,
             #[cfg(zcash_unstable = "zfuture")]
@@ -458,14 +458,25 @@ impl Parameters for TestNetwork {
 
     fn activation_height(&self, nu: NetworkUpgrade) -> Option<BlockHeight> {
         match nu {
-            NetworkUpgrade::Overwinter => Some(BlockHeight(207_500)),
-            NetworkUpgrade::Sapling => Some(BlockHeight(280_000)),
-            NetworkUpgrade::Blossom => Some(BlockHeight(584_000)),
-            NetworkUpgrade::Heartwood => Some(BlockHeight(903_800)),
-            NetworkUpgrade::Canopy => Some(BlockHeight(1_028_500)),
-            NetworkUpgrade::Nu5 => Some(BlockHeight(1_842_420)),
-            NetworkUpgrade::Nu6 => Some(BlockHeight(2_976_000)),
+            // NetworkUpgrade::Overwinter => Some(BlockHeight(207_500)),
+            // NetworkUpgrade::Sapling => Some(BlockHeight(280_000)),
+            // NetworkUpgrade::Blossom => Some(BlockHeight(584_000)),
+            // NetworkUpgrade::Heartwood => Some(BlockHeight(903_800)),
+            // NetworkUpgrade::Canopy => Some(BlockHeight(1_028_500)),
+            // NetworkUpgrade::Nu5 => Some(BlockHeight(1_842_420)),
+            // NetworkUpgrade::Nu6 => Some(BlockHeight(2_976_000)),
+            // NetworkUpgrade::Nu6_1 => Some(BlockHeight(3_536_500)),
+
+            // @TODO(JUDAH): Should be driven by regtest config!!!!!
+            NetworkUpgrade::Overwinter => Some(BlockHeight(1)),
+            NetworkUpgrade::Sapling => Some(BlockHeight(1)),
+            NetworkUpgrade::Blossom => Some(BlockHeight(1)),
+            NetworkUpgrade::Heartwood => Some(BlockHeight(1)),
+            NetworkUpgrade::Canopy => Some(BlockHeight(1)),
+            NetworkUpgrade::Nu5 => Some(BlockHeight(1)),
+            NetworkUpgrade::Nu6 => Some(BlockHeight(1)),
             NetworkUpgrade::Nu6_1 => Some(BlockHeight(3_536_500)),
+
             #[cfg(zcash_unstable = "nu7")]
             NetworkUpgrade::Nu7 => None,
             #[cfg(zcash_unstable = "zfuture")]
@@ -917,8 +928,12 @@ mod tests {
             BranchId::Nu6,
         );
         assert_eq!(
+            BranchId::for_height(&MAIN_NETWORK, BlockHeight(3_146_400)),
+            BranchId::Nu6_1,
+        );
+        assert_eq!(
             BranchId::for_height(&MAIN_NETWORK, BlockHeight(5_000_000)),
-            BranchId::Nu6,
+            BranchId::Nu6_1,
         );
     }
 }
--- zcash_primitives/src/transaction/txid.rs
+++ zcash_primitives/src/transaction/txid.rs
@@ -1,3 +1,5 @@
+#![allow(warnings)]
+
 use crate::encoding::{StateWrite, WriteBytesExt};
 use core::borrow::Borrow;
 use core::convert::TryFrom;
@@ -27,8 +29,8 @@ use zcash_protocol::value::Zatoshis;
 
 #[cfg(zcash_unstable = "zfuture")]
 use super::{
-    components::tze::{self, TzeIn, TzeOut},
     TzeDigests,
+    components::tze::{self, TzeIn, TzeOut},
 };
 
 /// TxId tree root personalization
@@ -83,7 +85,7 @@ pub(crate) fn transparent_prevout_hash<TransparentAuth: transparent::Authorizati
 ) -> Blake2bHash {
     let mut h = hasher(ZCASH_PREVOUTS_HASH_PERSONALIZATION);
     for t_in in vin {
-        t_in.prevout.write(&mut h).unwrap();
+        t_in.prevout().write(&mut h).unwrap();
     }
     h.finalize()
 }
@@ -95,7 +97,7 @@ pub(crate) fn transparent_sequence_hash<TransparentAuth: transparent::Authorizat
 ) -> Blake2bHash {
     let mut h = hasher(ZCASH_SEQUENCE_HASH_PERSONALIZATION);
     for t_in in vin {
-        h.write_u32_le(t_in.sequence).unwrap();
+        h.write_u32_le(t_in.sequence()).unwrap();
     }
     h.finalize()
 }
@@ -526,7 +528,7 @@ impl TransactionDigest<Authorized> for BlockTxCommitmentDigester {
         let mut h = hasher(ZCASH_TRANSPARENT_SCRIPTS_HASH_PERSONALIZATION);
         if let Some(bundle) = transparent_bundle {
             for txin in &bundle.vin {
-                txin.script_sig.write(&mut h).unwrap();
+                txin.script_sig().write(&mut h).unwrap();
             }
         }
         h.finalize()
