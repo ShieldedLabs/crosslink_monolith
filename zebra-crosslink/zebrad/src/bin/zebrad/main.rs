11de5d47aa4337b6047c37aab3f2904bca243e17
Andrew Reece <azmreece@gmail.com>
2026-06-03 10:53:29 +0100

    Try jemalloc

--- zebra-crosslink/zebrad/src/bin/zebrad/main.rs
+++ zebra-crosslink/zebrad/src/bin/zebrad/main.rs
@@ -2,6 +2,14 @@
 
 use zebrad::application::{boot, APPLICATION};
 
+#[cfg(not(target_env = "msvc"))]
+#[global_allocator]
+static ALLOC: tikv_jemallocator::Jemalloc = tikv_jemallocator::Jemalloc;
+
+// #[global_allocator]
+// static ALLOC: std::alloc::System = std::alloc::System;
+
+
 /// Process entry point for `zebrad`
 fn main() {
     boot(&APPLICATION);
