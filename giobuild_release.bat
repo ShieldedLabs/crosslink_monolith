ae49da05919a11215036e1d8ced5376eb50a2c2d
Phillip Trudeau-Tavara <pmttavara@protonmail.com>
2026-08-21 00:07:44 +0400

    Dev: Cleanup

--- giobuild_release.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-pushd %~dp0\zebra-crosslink\
-
-set RUSTFLAGS=-Awarnings
-cargo build -F viz_gui --release
-
-popd
--- giorun_release.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-pushd %~dp0\zebra-crosslink\
-
-set RUSTFLAGS=-Awarnings
-cargo run -F viz_gui --release
-
-popd
